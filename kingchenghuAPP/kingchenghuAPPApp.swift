20d408ade25692cd5d1178b42a0bd8ab6de97a90
Raylee0306-VMmacbookair <ray0306.lee@gmail.com>
2022-05-26 10:43:13 +0800

    Initial Commit

---
+++ kingchenghuAPP/kingchenghuAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  kingchenghuAPPApp.swift
+//  kingchenghuAPP
+//
+//  Created by RAY on 2022/5/26.
+//
+
+import SwiftUI
+
+@main
+struct kingchenghuAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
