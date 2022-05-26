4e41472a1f18bd5fcf21daa7d52ef5e392acfe9c
Raylee0306-RAYSURFACE <ray0306.lee@gmail.com>
2022-05-26 10:58:06 +0800

    1.0.0

--- kingchenghuAPP/ContentView.swift
+++ kingchenghuAPP/ContentView.swift
@@ -1,18 +1,116 @@
 //
 //  ContentView.swift
-//  kingchenghuAPP
+//  school project
 //
-//  Created by RAY on 2022/5/26.
+//  Created by Jack on 2022/3/15.
 //
+
 
 import SwiftUI
 
+
+
+    
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        NavigationView {
+            ScrollView{
+                VStack{
+                    HStack{
+                        Text("選取主題")
+                            .padding()
+                            .font(.largeTitle.bold())
+                        Spacer()
+                        NavigationLink {
+                            Text("Person View")
+                        } label: {
+                            Image(systemName: "gearshape")
+                                .padding()
+                                .font(.largeTitle)
+                                .foregroundColor(.blue)
+                        }
+                    }
+                    Spacer()
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing:20){
+                            NavigationLink{
+                                birdmenu()
+                            }label: {
+                                ZStack{
+                                    Image("bird")
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 350, height: 650)
+                                        .clipped()
+                                        .cornerRadius(30)
+                                    Text("水鳥")
+                                        .fontWeight(.bold)
+                                        .font(.system(size: 35, design: .rounded))
+                                        .foregroundColor(.black)
+                                        .padding(EdgeInsets(top: 0, leading: 125, bottom: 0, trailing: 125))
+                                        .cornerRadius(10)
+                                        .padding(EdgeInsets(top: 610, leading: 15, bottom: 0, trailing: 15))
+                                        .background(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.white]), startPoint: .top, endPoint: .bottom))
+                                        .cornerRadius(30)
+                                        .padding()
+                                }
+                            }
+                            
+                            NavigationLink{
+                                plant()
+                            }label: {
+                                ZStack{
+                                    Image("plant")
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 350, height: 650)
+                                        .clipped()
+                                        .cornerRadius(30)
+                                    Text("植物")
+                                        .fontWeight(.bold)
+                                        .font(.system(size: 35, design: .rounded))
+                                        .foregroundColor(.black)
+                                        .padding(EdgeInsets(top: 0, leading: 125, bottom: 0, trailing: 125))
+                                        .cornerRadius(10)
+                                        .padding(EdgeInsets(top: 610, leading: 15, bottom: 0, trailing: 15))
+                                        .background(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.white]), startPoint: .top, endPoint: .bottom))
+                                        .cornerRadius(30)
+                                        .padding()
+                                }
+                            }
+                            
+                            NavigationLink{
+                                bruh()
+                            }label: {
+                                ZStack{
+                                    Image("place")
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 350, height: 650)
+                                        .clipped()
+                                        .cornerRadius(30)
+                                    Text("地圖")
+                                        .fontWeight(.bold)
+                                        .font(.system(size: 35, design: .rounded))
+                                        .foregroundColor(.black)
+                                        .padding(EdgeInsets(top: 0, leading: 125, bottom: 0, trailing: 125))
+                                        .cornerRadius(10)
+                                        .padding(EdgeInsets(top: 610, leading: 15, bottom: 0, trailing: 15))
+                                        .background(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.white]), startPoint: .top, endPoint: .bottom))
+                                        .cornerRadius(30)
+                                        .padding()
+                                }
+                            }
+
+                        }//HStack
+                    }//scrollview
+                }//VStack
+            }//Scrollview
+        }//navigationview
+    }//var body: some view
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
